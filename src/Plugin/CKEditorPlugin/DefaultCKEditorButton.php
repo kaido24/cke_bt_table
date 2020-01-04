721972dc513cdad61b21e9801454790d42ce9afe
Kaido <toomingas.k@gmail.com>
2020-01-04 18:56:25 +0200

    add files

---
+++ src/Plugin/CKEditorPlugin/DefaultCKEditorButton.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\cke_bt_table\Plugin\CKEditorPlugin;
+
+use Drupal\ckeditor\CKEditorPluginBase;
+use Drupal\editor\Entity\Editor;
+
+/**
+ * Defines the "bt_table" plugin.
+ *
+ * NOTE: The plugin ID ('id' key) corresponds to the CKEditor plugin name.
+ * It is the first argument of the CKEDITOR.plugins.add() function in the
+ * plugin.js file.
+ *
+ * @CKEditorPlugin(
+ *   id = "bt_table",
+ *   label = @Translation("Table")
+ * )
+ */
+class DefaultCKEditorButton extends CKEditorPluginBase {
+
+  /**
+   * {@inheritdoc}
+   *
+   * NOTE: The keys of the returned array corresponds to the CKEditor button
+   * names. They are the first argument of the editor.ui.addButton() or
+   * editor.ui.addRichCombo() functions in the plugin.js file.
+   */
+  public function getButtons() {
+    // Make sure that the path to the image matches the file structure of
+    // the CKEditor plugin you are implementing.
+    $path = drupal_get_path('module', 'cke_bt_table') . '/js/plugins/bt_table';
+    return array(
+      'bt_table' => array(
+        'label' => t('Table'),
+        'image' => $path . '/icons/table.png',
+      ),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFile() {
+    // Make sure that the path to the plugin.js matches the file structure of
+    // the CKEditor plugin you are implementing.
+    return drupal_get_path('module', 'cke_bt_table') . '/js/plugins/bt_table/plugin.js';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  function isInternal() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  function getDependencies(Editor $editor) {
+    return array();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  function getLibraries(Editor $editor) {
+    return array();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfig(Editor $editor) {
+    return array();
+  }
+
+}
